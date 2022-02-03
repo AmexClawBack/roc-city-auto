fc1c37ed79e3a86d91c9ff8a6f5a5313ccf37668
Dillon King <Dillonk2012@gmail.com>
2022-02-03 12:38:28 -0600

    Created the about component. It now needs styling

--- src/Pages/About/Index.js
+++ src/Pages/About/Index.js
@@ -2,9 +2,37 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            
-        </div>
+        <main className="page">
+        <section className="clean-block about-us">
+            <div className="container">
+                <div className="block-heading">
+                    <h2 className="text-info">About Us</h2>
+                    <p>We are a team of dedicated car professionals with over 30 years of experience helping get people into cash cars</p>
+                </div>
+                <div className="row justify-content-center">
+                    
+                    <div className="col-sm-6 col-lg-4">
+                        <div className="card text-center clean-card"><img className="card-img-top w-100 d-block" src="assets/img/avatars/avatar2.jpg"/>
+                            <div className="card-body info">
+                                <h4 className="card-title">Terrick</h4>
+                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                <div className="icons"><a href="#"><i className="icon-social-facebook"></i></a><a href="#"><i className="icon-social-instagram"></i></a><a href="#"><i className="icon-social-twitter"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-6 col-lg-4">
+                        <div className="card text-center clean-card"><img className="card-img-top w-100 d-block" src="assets/img/avatars/avatar3.jpg"/>
+                            <div className="card-body info">
+                                <h4 className="card-title">Brian</h4>
+                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                <div className="icons"><a href="#"><i className="icon-social-facebook"></i></a><a href="#"><i className="icon-social-instagram"></i></a><a href="#"><i className="icon-social-twitter"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
     );
 }
 
--- src/Pages/Contact/Index.js
+++ src/Pages/Contact/Index.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            
-        </div>
+        <main className="page contact-us-page">
+        <section className="clean-block clean-form dark">
+            <div className="container">
+                <div className="block-heading">
+                    <h2 className="text-info">Contact Us</h2>
+                    <p>We will respond to you as soon as possible!</p>
+                </div>
+                <form>
+                    <div className="mb-3"><label className="form-label" htmlFor="name">Name</label><input className="form-control" type="text" id="name" name="name"/></div>
+                    <div className="mb-3"><label className="form-label" htmlFor="subject">Subject</label><input className="form-control" type="text" id="subject" name="subject"/></div>
+                    <div className="mb-3"><label className="form-label" htmlFor="email">Email</label><input className="form-control" type="email" id="email" name="email"/></div>
+                    <div className="mb-3"><label className="form-label" htmlFor="message">Message</label><textarea className="form-control" id="message" name="message"></textarea></div>
+                    <div className="mb-3"><button className="btn btn-primary" type="submit">Send</button></div>
+                </form>
+            </div>
+        </section>
+    </main>
     );
 }
 
