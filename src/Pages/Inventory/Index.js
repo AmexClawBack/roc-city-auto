e443129a2447cd07c3c6dff661cfeb6276e29e2d
Dillon King <Dillonk2012@gmail.com>
2022-02-03 12:23:45 -0600

    Created the inventory page component. It now needs styling

--- src/Pages/Inventory/Index.js
+++ src/Pages/Inventory/Index.js
@@ -2,9 +2,77 @@ import React from 'react';
 
 const Inventory = () => {
     return (
-        <div>
-            
-        </div>
+        <main className="page catalog-page">
+        <section className="clean-block clean-catalog dark">
+            <div className="container">
+                <div className="block-heading">
+                    <h2 className="text-info">Inventory</h2>
+                    <p>Browse our entire inventory of used cash cars</p>
+                </div>
+                <div className="content"></div>
+                <div className="card-group">
+                    <div className="card"><img className="card-img-top w-100 d-block"/>
+                        <div className="card-body">
+                            <h4 className="card-title">Title</h4>
+                            <p className="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button className="btn btn-primary" type="button">Button</button>
+                        </div>
+                    </div>
+                    <div className="card"><img className="card-img-top w-100 d-block"/>
+                        <div className="card-body">
+                            <h4 className="card-title">Title</h4>
+                            <p className="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button className="btn btn-primary" type="button">Button</button>
+                        </div>
+                    </div>
+                    <div className="card"><img className="card-img-top w-100 d-block"/>
+                        <div className="card-body">
+                            <h4 className="card-title">Title</h4>
+                            <p className="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button className="btn btn-primary" type="button">Button</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-group">
+                    <div className="card"><img className="card-img-top w-100 d-block"/>
+                        <div className="card-body">
+                            <h4 className="card-title">Title</h4>
+                            <p className="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button className="btn btn-primary" type="button">Button</button>
+                        </div>
+                    </div>
+                    <div className="card"><img className="card-img-top w-100 d-block"/>
+                        <div className="card-body">
+                            <h4 className="card-title">Title</h4>
+                            <p className="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button className="btn btn-primary" type="button">Button</button>
+                        </div>
+                    </div>
+                    <div className="card"><img className="card-img-top w-100 d-block"/>
+                        <div className="card-body">
+                            <h4 className="card-title">Title</h4>
+                            <p className="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button className="btn btn-primary" type="button">Button</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-group">
+                    <div className="card"><img className="card-img-top w-100 d-block"/>
+                        <div className="card-body">
+                            <h4 className="card-title">Title</h4>
+                            <p className="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button className="btn btn-primary" type="button">Button</button>
+                        </div>
+                    </div>
+                    <div className="card"><img className="card-img-top w-100 d-block"/>
+                        <div className="card-body">
+                            <h4 className="card-title">Title</h4>
+                            <p className="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button className="btn btn-primary" type="button">Button</button>
+                        </div>
+                    </div>
+                    <div className="card"><img className="card-img-top w-100 d-block"/>
+                        <div className="card-body">
+                            <h4 className="card-title">Title</h4>
+                            <p className="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button className="btn btn-primary" type="button">Button</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
     );
 }
 
